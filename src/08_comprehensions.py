cca0a635fd26f955042d9f51f199d0cccd15e75b
masonnystrom <masonnystrom@gmail.com>
2020-07-09 23:37:44 -0400

    update with rps

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -2,7 +2,7 @@
 List comprehensions are one cool and unique feature of Python.
 They essentially act as a terse and concise way of initializing
 and populating a list given some expression that specifies how
-the list should be populated. 
+the list should be populated.
 
 Take a look at https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
 for more info regarding list comprehensions.
@@ -11,31 +11,36 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
-
+for num in range(5):
+    y.append(num)
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-
-print(y)
+y2 = []
+for num in range(0,9):
+    y2.append(num **2)
+print(y2)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
-
+y3 = []
 a = ["foo", "bar", "baz"]
 
-y = []
+for string in a:
+    y3.append(string.upper())
 
-print(y)
+print(y3)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
-
+x_even = []
 x = input("Enter comma-separated numbers: ").split(',')
+print(x)
 
-# What do you need between the square brackets to make it work?
-y = []
-
-print(y)
+for num in x:
+    num = int(num)
+    if num % 2 == 0:
+        x_even.append(num)
+print(x_even)
--- src/10_functions.py
+++ src/10_functions.py
@@ -1,12 +1,57 @@
+import random
 # Write a function is_even that will return true if the passed-in number is even.
-
-# YOUR CODE HERE
-
-# Read a number from the keyboard
-num = input("Enter a number: ")
-num = int(num)
+def mult2(x):
+    return x * 2
+print(mult2)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
-# YOUR CODE HERE
+def iseven(num):
+    if num % 2 == 0:
+        return "Number is Even"
+    else:
+        return "Number is Odd"
+
+# Read a number from the keyboard
+# num = input("Enter a number: ")
+# num = int(num)
+#print(iseven(num))
+
+# doubling function that passes arg by reference
+l = [1, 2, 3, 4]
+print(l)
+
+def mult2_list(l):
+    for i in range(len(l)):
+        l[i] *= 2
+    return l
+y = mult2_list(l)
+print(y)
+
+# or
+# def mult2_list(x):
+#     for i in l:
+#         i *= 2
+
+# rock paper scissors
+wins = 0
+losses = 0
+ties = 0
+
+# possible_choices = ["rock" : 1, "paper": 2, "scissors": 3]
+choices = ["rock", "paper", "scissors"]
+user_choice = input("Choose rock, paper, or scissors")
+computer_choice = random.randint(0,2)
+print(computer_choice)
+
+if user_choice == "rock":
+    if computer_choice == "rock":
+        print("it's a tie")
+        ties += 1
+    elif computer_choice == "paper":
+        print("you lose!")
+        losses += 1
+    else:
+        print("You won!")
+        wins += 1
 
